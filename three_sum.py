f5de79ca5c1d1d9d7747678d0990ffb6926590b4
Rutuj18 <117572304+Rutuj18@users.noreply.github.com>
2025-01-10 19:54:23 +0530

    Create three_sum.py

---
+++ three_sum.py
@@ -0,0 +1,35 @@
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+    result = set()
+    neg, pos, zero = [], [], []
+
+    for x in nums:
+        if x > 0:
+            pos.append(x)
+        elif x < 0:
+            neg.append(x)
+        else:
+            zero.append(x)
+
+    neg_set, pos_set = set(neg), set(pos)
+
+    if zero:
+        for x in pos:
+            if -x in neg_set:
+                result.add((-x, 0, x))
+
+    if len(zero) >= 3:
+        result.add((0, 0, 0))
+
+    for i in range(len(neg)):
+        for j in range(i + 1, len(neg)):
+            complement = -1 * (neg[i] + neg[j])
+            if complement in pos_set:
+                result.add(tuple(sorted([neg[i], neg[j], complement])))
+
+    for i in range(len(pos)):
+        for j in range(i + 1, len(pos)):
+            complement = -1 * (pos[i] + pos[j])
+            if complement in neg_set:
+                result.add(tuple(sorted([pos[i], pos[j], complement])))
+
+    return result
