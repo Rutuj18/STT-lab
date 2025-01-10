4c2efbb45070e70894c31345ba98a96e4e093094
Rutuj18 <117572304+Rutuj18@users.noreply.github.com>
2025-01-10 20:48:40 +0530

    Update three_sum.py

--- three_sum.py
+++ three_sum.py
@@ -15,21 +15,20 @@ def three_sum(nums: list[int]) -> list[list[int]]:
         if x > 0:
             pos.append(x)
         elif x < 0:
-            neg.append(x)
-        else:
-            zero.append(x)
+            neg.append(x)   
     neg_set, pos_set = set(neg), set(pos)
-    if zero:
-        for x in pos:
-            if -x in neg_set:
-                result.add((-x, 0, x))
-    if len(zero) >= 3:
-        result.add((0, 0, 0))
+    
+    if 0 in nums:
+        result.update({(-x, 0, x) for x in pos if -x in neg_set})
+        if nums.count(0) >= 3:
+           result.add((0, 0, 0))
+        
     for i, num1 in enumerate(neg):
         for num2 in neg[i+1]:
             complement = -1 * (num1 + num2)
             if complement in pos_set:
                 result.add(tuple(sorted([num1,num2, complement])))
+                
     for i, num1 in enumerate(pos):
         for num2 in pos[i+1]:
             complement = -1 * (num1 + num2)
