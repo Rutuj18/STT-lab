88dadbb2764bf5cac972eeca3384503462457ebf
Rutuj18 <117572304+Rutuj18@users.noreply.github.com>
2025-01-10 20:49:47 +0530

    Update three_sum.py

--- three_sum.py
+++ three_sum.py
@@ -1,11 +1,9 @@
 """
 Module for solution of 3 Sum problem
-
 """
 def three_sum(nums: list[int]) -> list[list[int]]:
     """
     Finds the unique triplets from given array of numbers having sum zero
-    
     input: A list of numbers 
     output: A list contanining triplets having sum zero
     """
@@ -17,18 +15,15 @@ def three_sum(nums: list[int]) -> list[list[int]]:
         elif x < 0:
             neg.append(x)   
     neg_set, pos_set = set(neg), set(pos)
-    
     if 0 in nums:
         result.update({(-x, 0, x) for x in pos if -x in neg_set})
         if nums.count(0) >= 3:
-           result.add((0, 0, 0))
-        
+           result.add((0, 0, 0))    
     for i, num1 in enumerate(neg):
         for num2 in neg[i+1]:
             complement = -1 * (num1 + num2)
             if complement in pos_set:
-                result.add(tuple(sorted([num1,num2, complement])))
-                
+                result.add(tuple(sorted([num1,num2, complement])))          
     for i, num1 in enumerate(pos):
         for num2 in pos[i+1]:
             complement = -1 * (num1 + num2)
